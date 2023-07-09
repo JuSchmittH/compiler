0607a00644fa1976cd5888384ecd50e96b6aa99c
Luma Beserra <lumabeserra@Lumas-MacBook-Pro.local>
2023-07-09 18:40:40 -0300

    fix vl

--- etapa3/vl.c
+++ etapa3/vl.c
@@ -8,9 +8,9 @@ VL *vl_new(int line_number, char *token_type, char *token_value){
   VL *valor_lexico = NULL;
   valor_lexico = calloc(1, sizeof(VL));
   if (valor_lexico != NULL){
-    valor_lexico->line_number = ;
+    valor_lexico->line_number = line_number;
     valor_lexico->token_type = token_type;
-    valor_lexico->token_value = strdup(label);
+    valor_lexico->token_value = strdup(token_value);
   }
   return valor_lexico;
 }
