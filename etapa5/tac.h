6f6ee21209f1cc629fc9e9687b1531ed1f6beb46
Juliana Henrique <juliana.schmitth@gmail.com>
2023-08-19 11:29:57 -0300

    create tac files

---
+++ etapa5/tac.h
@@ -0,0 +1,7 @@
+//Implementar duas estruturas de dados: uma para representar uma operação ILOC e os argumentos necessários para ela;
+//outra para manter uma lista de operações ILOC.
+
+typedef struct ILOC_operation
+{
+    
+} ILOC_Op;
